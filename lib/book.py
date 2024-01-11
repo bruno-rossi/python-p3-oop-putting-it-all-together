e8a00c9c81838ac098890ae09714f6917c769f14
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-11 14:55:03 -0500

    Shoes and books

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
     
-        
+    @page_count.setter
+    def page_count(self, page_count):
+        """page_count setter"""
+        if isinstance(page_count, int) == True:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(page):
+        print("Flipping the page...wow, you read fast!")
+
+new_book = Book("Title", 80)
+print(new_book.page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    # Constructor method:
+    def __init__(self, brand, size): 
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        if isinstance(size, int) == True:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
